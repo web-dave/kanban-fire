73b805d9e428c3276b4e8e3ef5bc7b06bbb2f457
web-dave <dave@webdave.de>
2022-10-18 22:55:19 +0200

    remove creds

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyCuUHdYuF2pXU5fhODdhFy3t8VmCkznJaU',
-    authDomain: 'kanbanfire-ddb91.firebaseapp.com',
-    projectId: 'kanbanfire-ddb91',
-    storageBucket: 'kanbanfire-ddb91.appspot.com',
-    messagingSenderId: '681830283024',
-    appId: '1:681830283024:web:0a88cee4bce49a3bdb5795',
+    apiKey: '---',
+    authDomain: '---',
+    projectId: '---',
+    storageBucket: '---',
+    messagingSenderId: '---',
+    appId: '---',
   },
 };
 
